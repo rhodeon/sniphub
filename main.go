2c75e380d173949f0197bc0527d82d5345eab88a
rhodeon <ruonabraks@gmail.com>
2022-02-03 14:03:14 +0100

    Added snippet display and creation handlers.

--- main.go
+++ main.go
@@ -9,12 +9,24 @@ const PORT = "4000"
 
 // default home response
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to Texthub"))
+	w.Write([]byte("Welcome to Texthub."))
+}
+
+// shows an example snippet
+func showSnippet(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("This is an example snippet."))
+}
+
+// allows user to create a snippet
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("You can create a snippet here."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/", showSnippet)
+	mux.HandleFunc("/snippet/create/", createSnippet)
 
 	log.Println("Listening on port", PORT)
 	err := http.ListenAndServe(":"+PORT, mux)
