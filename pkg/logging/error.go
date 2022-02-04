bdc52243212d5595137cfe0b6d22c92f84443c70
rhodeon <ruonabraks@gmail.com>
2022-02-04 17:58:08 +0100

    Updated logging package to support more print variants.

---
+++ pkg/logging/error.go
@@ -0,0 +1,49 @@
+package logging
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	colors "github.com/rhodeon/sniphub/pkg/color"
+)
+
+// Logger which prints message to standard error.
+// Also displays the date, time and relative path of the affected file.
+var errorLogger = log.New(os.Stderr, "ERROR:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+// Prints error log and resets text colour afterwards.
+func colorizeError(logError func(), error ...interface{}) {
+	fmt.Fprint(os.Stderr, colors.Red)
+	logError()
+	fmt.Fprint(os.Stderr, colors.Reset)
+}
+
+// Equivalent to fmt.Print.
+func Error(error ...interface{}) {
+	colorizeError(func() {
+		errorLogger.Print(error...)
+	},
+	)
+}
+
+// Equivalent to fmt.PrintF.
+func ErrorF(format string, error ...interface{}) {
+	colorizeError(func() {
+		errorLogger.Printf(format, error...)
+	})
+}
+
+// Equivalent to fmt.PrintLn.
+func ErrorLn(error ...interface{}) {
+	colorizeError(func() {
+		errorLogger.Println(error...)
+	})
+}
+
+// Logs the error then exits the program.
+func FatalError(error ...interface{}) {
+	colorizeError(func() {
+		errorLogger.Fatal(error...)
+	})
+}
---
+++ pkg/logging/info.go
@@ -0,0 +1,41 @@
+package logging
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	colors "github.com/rhodeon/sniphub/pkg/color"
+)
+
+// Logger which prints message to standard output.
+// Also displays the date and time.
+var infoLogger = log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
+
+// Prints info log and resets text colour afterwards.
+func colorizeInfo(logInfo func(), info ...interface{}) {
+	fmt.Fprint(os.Stdout, colors.Yellow)
+	logInfo()
+	fmt.Fprint(os.Stdout, colors.Reset)
+}
+
+// Equivalent to fmt.Print.
+func Info(info ...interface{}) {
+	colorizeInfo(func() {
+		infoLogger.Print(info...)
+	}, info...)
+}
+
+// Equivalent to fmt.Printf.
+func InfoF(format string, info ...interface{}) {
+	colorizeInfo(func() {
+		infoLogger.Printf(format, info...)
+	})
+}
+
+// Equivalent to fmt.Println.
+func InfoLn(format string, info ...interface{}) {
+	colorizeInfo(func() {
+		infoLogger.Println(info...)
+	})
+}
