a74a17299310c8f8dc4f03393445cb856ed55b1a
rhodeon <ruonabraks@gmail.com>
2022-02-09 20:07:21 +0100

    Added middleware to log requests.

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,12 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/rhodeon/sniphub/pkg/prettylog"
+)
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(rw http.ResponseWriter, r *http.Request) {
 			rw.Header().Set("X-Frame-Options", "deny")
 			rw.Header().Set("X-XSS-Protection", "1; mode=block")
+			next.ServeHTTP(rw, r)
+		},
+	)
+}
+
+func logRequests(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(rw http.ResponseWriter, r *http.Request) {
+			prettylog.InfoF("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 			next.ServeHTTP(rw, r)
 		},
 	)
